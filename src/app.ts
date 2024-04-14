07f6b5e74ba188f7bcb8b1d1c22d0d688b7421fd
Swapnil Bajpai <swappy20@proton.me>
2024-04-14 22:55:58 +0530

    Initial Comit

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
